3ac4ae2692a0a19547f11d69ea6b8e98bcd907df
Kyle M <kemallette@gmail.com>
2013-08-19 22:11:15 -0500

    Init

---
+++ src/com/kemallette/MultiChoiceExpandableList/Activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.kemallette.MultiChoiceExpandableList.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.kemallette.multichoiceexpandablelist.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle arg0){
+
+		super.onCreate(arg0);
+		
+		setContentView(R.layout.main_activity);
+	}
+	
+}
